f79a9fff002e9d1785b9d13ac18b2661093041a5
Нинуля <demchenko.nina2004@gmail.com>
2024-05-28 17:28:39 +0700

    День недели

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -9,12 +9,35 @@ function showDate() {
                     Дата и время для корейской локали: ${today.toLocaleString('ko-KR')}<br>         
                     Дата и время для малазийской локали: ${today.toLocaleString('ms-MY')}<br>`;
                     
-    let date = document.getElementById('date');                
+    let date = document.getElementById('date');
+    const month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
     const weekDay = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-    const month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
 
     date.innerHTML = `Текущий день: ${today.getDate()}<br>
                     Текущий месяц: ${month[today.getMonth()]}<br>
                     Текущий год: ${today.getFullYear()}<br>
                     Текущий день недели: ${weekDay[today.getDay()]}`;
+}
+
+function getWeekDay() {
+    const weekDay = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    let day = document.getElementById('day').value;
+    let month = document.getElementById('month').value - 1;
+    let year = document.getElementById('year').value;
+
+    let date = new Date(year, month, day);   
+    let getDay = date.getDay();
+
+    let dayOFWeek = document.getElementById('getWeekDay');
+    dayOFWeek.textContent = `День недели: ${weekDay[getDay]}`;
+
+    if (day < 1 || day > 31 || isNaN(day)) {
+        dayOFWeek.innerText = "Ошибка! Убедитесь, что поле заполнено корректно.";        
+    }
+    else if (month < 1 || month > 12 || isNaN(month)) {
+        dayOFWeek.innerText = "Ошибка! Убедитесь, что поле заполнено корректно.";
+    }
+    else if (year < 1699 || isNaN(year)) {
+        dayOFWeek.innerText = "Ошибка! Убедитесь, что поле заполнено корректно.";        
+    }  
 }
