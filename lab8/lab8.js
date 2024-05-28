188a16def3202fbb8e608b68c6f81f00e7765bdd
Нинуля <demchenko.nina2004@gmail.com>
2024-05-28 15:53:46 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -7,5 +7,14 @@ function showDate() {
                     Дата и время для египетской локали: ${today.toLocaleString('ar-EG')}<br>                    
                     Дата и время для индийской локали: ${today.toLocaleString('mr-IN')}<br>                                 
                     Дата и время для корейской локали: ${today.toLocaleString('ko-KR')}<br>         
-                    Дата и время для малазийской локали: ${today.toLocaleString('ms-MY')}<br>`
+                    Дата и время для малазийской локали: ${today.toLocaleString('ms-MY')}<br>`;
+                    
+    let date = document.getElementById('date');                
+    const weekDay = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    const month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
+
+    date.innerHTML = `Текущий день: ${today.getDate()}<br>
+                    Текущий месяц: ${month[today.getMonth()]}<br>
+                    Текущий год: ${today.getFullYear()}<br>
+                    Текущий день недели: ${weekDay[today.getDay()]}`;
 }
