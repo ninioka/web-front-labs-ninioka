9f289231558d75c10b7eeaebaa1899eb871b01cc
Нинуля <demchenko.nina2004@gmail.com>
2024-06-03 01:08:31 +0700

    Аналоговые часы

--- lab8/time.js
+++ lab8/time.js
@@ -2,5 +2,15 @@ function showTime() {
     let today = new Date();
     let currentTime = today.toLocaleTimeString('ru-RU');
     document.getElementById('time').innerHTML = currentTime;
+
+    const seconds = today.getSeconds();
+    const secondsDisplay = document.getElementById('second');
+    const secondHand = document.getElementById('stick');
+
+   
+    secondsDisplay.innerHTML = seconds.toString().padStart(2, '0');
+   
+    const secondsDegree = ((seconds / 60) * 360) + 90; 
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`;
 }
 setInterval(showTime, 1000);
